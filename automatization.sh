7ed1d77d9a09a0c66b313865916ad455b038aafc
NatalyaGolt <golt-natalya@yandex.ru>
2022-05-10 20:07:49 +0300

    add automatization.sh

---
+++ automatization.sh
@@ -0,0 +1,6 @@
+cd folder_name_1
+mkdir folder_name_2 folder_name_3 folder_name_4
+cd folder_name_4
+touch file_1.txt file_2.txt file_3.txt file_4.json file_5.json
+mkdir folder_name_5 folder_name_6 folder_name_7
+ls -la
